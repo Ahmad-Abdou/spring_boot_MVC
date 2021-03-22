7dd73df03aad3c0ead2f9790dfeacd96de889f11
Ahmad-Abdou <47502597+Ahmad-Abdou@users.noreply.github.com>
2021-03-22 12:07:29 +0100

    Adding Attributes to Controller

--- src/main/java/com/lexicon/spring_mvc/controller/IndexController.java
+++ src/main/java/com/lexicon/spring_mvc/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.lexicon.spring_mvc.controller;
 
+import org.dom4j.rule.Mode;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,14 +10,17 @@ public class IndexController {
 
 
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+        String home = "Welcome to Home";
+       model.addAttribute("home",home);
         return "home";
     }
 
     @GetMapping("/index")
-    public String goToIndexPage(){
-
-        return "index";
+    public String goToIndexPage(Model model){
+        String index = "Welcome to index";
+        model.addAttribute("index",index);
+        return "index" ;
     }
 
 
