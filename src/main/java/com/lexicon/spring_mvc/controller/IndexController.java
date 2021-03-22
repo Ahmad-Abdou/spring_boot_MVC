b0c5e61378deadb5ac6612a2c706a86891072f04
Ahmad-Abdou <47502597+Ahmad-Abdou@users.noreply.github.com>
2021-03-22 11:06:06 +0100

    Adding home dir and index

---
+++ src/main/java/com/lexicon/spring_mvc/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.lexicon.spring_mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/index")
+    public String goToIndexPage(){
+
+        return "index";
+    }
+
+
+}
